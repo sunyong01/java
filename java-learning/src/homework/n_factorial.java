48e25877397a3c8a14216d63a947e89f4764ffb9
sunyong01 <2909665726@qq.com>
2019-09-26 22:43:08 +0800

    阶乘破long

--- java-learning/src/homework/n_factorial.java
+++ java-learning/src/homework/n_factorial.java
@@ -8,12 +8,13 @@ public class n_factorial {
 
     private static class factorial {
         private long number = 1;
-        private int count = 1;
+        private int count = 0;
 
 
         private long tester() {
-            number = number * count;
             count++;
+            number = number * count;
+
             return number;
         }
     }
@@ -23,7 +24,7 @@ public class n_factorial {
         long big_number;
         while (true) {
             big_number = test.tester();
-            System.out.println(big_number);
+            System.out.println(test.count+"的阶乘"+big_number);
             if (big_number < 0) {
                 System.out.println(test.count  + "的阶乘>long的最大值");
                 System.out.println("系统支持的最大long类型的大小：" + Long.MAX_VALUE);
