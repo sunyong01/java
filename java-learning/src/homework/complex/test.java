b5c19e35026bfc97d302d8ff42484975c0a7db9f
sunyong01 <2909665726@qq.com>
2019-10-09 20:49:54 +0800

    复数运算

--- java-learning/src/homework/complex/test.java
+++ java-learning/src/homework/complex/test.java
@@ -3,8 +3,8 @@ package homework.complex;
 import java.util.Scanner;
 
 class base{
-    double x=0;
-    double y=0;
+    double x;
+    double y;
     base(double x,double y){
         this.x=x;
         this.y=y;
@@ -14,7 +14,7 @@ class complex{
     base complexInput(){ Scanner raw =new Scanner(System.in);
         double raw_x=raw.nextDouble();
         String raw_y=raw.nextLine();
-        double y=Double.valueOf(raw_y.replace("i",""));
+        double y=Double.parseDouble(raw_y.replace("i",""));
         return new base(raw_x,y);
     }
     base add(base b1,base b2){
@@ -24,16 +24,16 @@ class complex{
         return new base(b1.x-b2.x,b1.y-b2.y);
     }
     base multiply(base b1,base b2){
-        /**
-         *  a=b1.x  b=b1.y   c=b2.x  d=b2.y
-         *z1=a+bi，z2=c+di
-         *(ac－bd)+(bc+ad)i
-         *
+        /*
+           a=b1.x  b=b1.y   c=b2.x  d=b2.y
+         z1=a+bi，z2=c+di
+         (ac－bd)+(bc+ad)i
+
          */
         return new base((b1.x*b2.x)-(b1.y*b2.y),(b1.y*b2.x)+(b1.x*b2.y));
     }
     base division(base b1,base b2){
-        /**
+        /*
          *      a=b1.x  b=b1.y   c=b2.x  d=b2.y
          *   (a+bi)/(c+di)=(ac+bd)/(c^2+d^2) +((bc-ad)/(c^2+d^2))i
          */
@@ -45,7 +45,6 @@ class complex{
         System.out.println(bs.x+"+"+bs.y+"i");
     }
 }
-
 class run{
     public static void main(String[] args) {
         System.out.println("请输入两个复数 复数中间空格为界如a+bi输入a bi，若为实数输入a 0i");
